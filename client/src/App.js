cfdb8ea226ac6c7b4d28a097643193966b4786f6
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-01-27 15:21:22 -0500

    finally got something to display

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Drivetrain from './components/Drivetrain';
+
+function App() {
+  return (
+    <Drivetrain />
+  );
+}
+
+export default App;
---
+++ client/src/components/Drivetrain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Drivetrain() {
+  return (
+    <div>
+      <h1>Drivetrain</h1>
+    </div>
+  );
+}
+
+export default Drivetrain;
